322d7c75b274a0bf0275bebda0a3c58e180c7d50
Ritik <negiritik47@gmail.com>
2025-03-23 11:19:04 +0530

    finishing login component

--- src/app/components/form-analytics/form-analytics.component.ts
+++ src/app/components/form-analytics/form-analytics.component.ts
@@ -1,3 +1,26 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-form-analytics',
+//   templateUrl: './form-analytics.component.html',
+//   styleUrls: ['./form-analytics.component.scss']
+// })
+// export class FormAnalyticsComponent {
+//   responses = ['Response 1', 'Response 2','Response 3', 'Response 4','Response 5', 'Response 6']; // Example responses
+
+//   questions = [
+//     {
+//       text: 'Responses',
+//       responses: this.responses
+//     }
+//   ];
+//   viewResponse(response: string, index: number) {
+//     alert(`Response #${index + 1}: ${response}`);
+//   }
+
+// }
+
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,16 +29,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-analytics.component.scss']
 })
 export class FormAnalyticsComponent {
-  responses = ['Response 1', 'Response 2','Response 3', 'Response 4','Response 5', 'Response 6']; // Example responses
+  responses = ['Response 1', 'Response 2', 'Response 3', 
+              'Response 4', 'Response 5', 'Response 6'];
 
   questions = [
     {
-      text: 'Responses',
+      text: 'Survey Responses Overview',
       responses: this.responses
     }
   ];
-  viewResponse(response: string, index: number) {
-    alert(`Response #${index + 1}: ${response}`);
-  }
 
+  viewResponse(response: string, index: number) {
+    alert(`Response #${index + 1}:\n\n${response}`);
+  }
 }
