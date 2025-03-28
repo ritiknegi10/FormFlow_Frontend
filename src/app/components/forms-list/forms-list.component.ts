5a6383823e27451d259e395e1471f76b88d7c321
Namrata-2424 <namratakumari730@gmail.com>
2025-03-28 11:53:39 +0530

    Added pop-up before deleting a form.

--- src/app/components/forms-list/forms-list.component.ts
+++ src/app/components/forms-list/forms-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormService } from '../../services/form.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -29,4 +30,23 @@ export class FormsListComponent implements OnInit {
   deleteForm(index: number) {
     this.formService.deleteForm(index);
   }
+
+  confirmDeleteForm(formId: number) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This action cannot be undone!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.deleteForm(formId);
+        Swal.fire('Deleted!', 'The form has been deleted.', 'success');
+      }
+    });
+  }
+  
+
 }
